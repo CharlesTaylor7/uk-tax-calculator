2560c6213fb549dba137497ea240c24b393042e3
Charles Taylor <charlestaylor95@gmail.com>
2025-05-19 08:50:10 -0400

    Works

--- frontend/e2e/calculator.spec.ts
+++ frontend/e2e/calculator.spec.ts
@@ -10,15 +10,15 @@ test('calculator should calculate tax correctly', async ({ page }) => {
   // Click the calculate button
   await page.click('button[name="calculate"]');
 
-  // Check that the results are displayed
+  // Check that all 6 summary results are displayed
   await expect(page.locator('.calculator__result-row')).toHaveCount(6);
 
-  // Check that the tax amount is displayed correctly
+  // Check the annual tax amount is displayed correctly
   await expect(
     page
       .locator('.calculator__result-row', {
         hasText: 'Annual Tax Paid:',
       })
-      .locator('.calculator__result-value')
-  ).toContainText('£3,000.00');
+      .locator('.calculator__result-value'),
+  ).toContainText('£7,000.00');
 });
