e9bbc4eff288310709cdf32a487e6cccc4f9f3c2
Charles Taylor <charlestaylor95@gmail.com>
2025-05-19 11:30:00 -0400

    Task failed succesfully

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -2,20 +2,23 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './e2e',
-  fullyParallel: true,
-  retries: 2,
+  retries: 1,
   workers: 1,
   reporter: 'html',
+  // timeout in 5s for user actions, navigation and DOM asserts
+  expect: {
+    timeout: 5000,
+  },
   use: {
-    // Use the app container name instead of localhost
-    baseURL: process.env.CI ? 'http://localhost:8080' : 'http://localhost:4200',
-    // Increase timeouts to allow for container startup
-    navigationTimeout: 60000,
-    actionTimeout: 30000,
+    actionTimeout: 5000,
+    navigationTimeout: 5000,
     // Other settings
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
     ignoreHTTPSErrors: true,
+
+    // allow running against docker image or local dev server
+    baseURL: process.env.CI ? 'http://localhost:8080' : 'http://localhost:4200',
   },
   projects: [
     {
